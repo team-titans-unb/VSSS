4c840a816619d33a14eaa1b756ef6701a7195116
Luiz Felipe <luizcn123@hotmail.com>
2024-08-11 12:22:03 -0300

    update

--- competition-code/main/testMatchuano.py
+++ competition-code/main/testMatchuano.py
@@ -1,5 +1,6 @@
 from robotMonitor import *
 
+# Este é um exemplo de como vc pode fazer o envio de dados para o robo
 esp_configs = {
         'esp1': {'ip': '192.168.33.253', 'port': 80, 'start_msg': 1, 'stop_msg': 0}
     }
